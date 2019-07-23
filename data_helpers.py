1616488d6a43f82dc087bc8091a4e2729ffdb1df
Ada520 <1039956242@qq.com>
2019-07-23 17:22:10 +0800

    定义数据预处理文件data_helpers

--- data_helpers.py
+++ data_helpers.py
@@ -6,3 +6,4 @@ def clean_str(string):
     Tokenization/string cleaning for all datasets except for SST
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
+    string=re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
