3fa075c96fa683291c8a81ad389fa445a5086e56
Ada520 <1039956242@qq.com>
2019-07-23 17:17:34 +0800

    定义数据预处理文件data_helpers

--- data_helpers.py
+++ data_helpers.py
@@ -1,2 +1,8 @@
 import numpy as np
 import re
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
